fbce91ff21aaf41df36e177de154b04f88d246dc
Point-SimoSiren <45048404+Point-SimoSiren@users.noreply.github.com>
2021-01-30 12:09:44 +0200

    Tyylittelyä client konsolisovellukseen jolla dataa voi lukea läppärillä

--- Program.cs
+++ Program.cs
@@ -8,14 +8,25 @@ namespace coreConsoleApp
     {
         static async Task Main(string[] args)
         {
-            
-            HttpClient client = new HttpClient();
+            while (1 == 1)
+            {
+                HttpClient client = new HttpClient();
 
-            string uri = "https://timesheetrest.azurewebsites.net/api/measurements";
+                string uri = "https://timesheetrest.azurewebsites.net/api/measurements";
 
-            string responseBody = await client.GetStringAsync(uri);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Cyan;
 
-            Console.WriteLine(responseBody);
+                Console.WriteLine("Ladataan Raspberryn dataa... hetkinen....");
+
+                string responseBody = await client.GetStringAsync(uri);
+
+                Console.Write(responseBody);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Enterillä data päivittyy, sammutus sulkemalla ikkuna");
+                Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+            }
 
         }
     }
